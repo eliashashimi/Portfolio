8e7d52446b5752cb268918ff02b917bceb6f0a34
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-09-26 19:04:47 +0200

    added main-content component and added in app.html and app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Hero } from './layout/components/hero/hero';
+import { MainContent } from './layout/components/main-content/main-content';
 
 @Component({
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, MainContent],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
---
+++ src/app/layout/components/main-content/main-content.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-main-content',
+  styleUrl: './main-content.scss',
+  templateUrl: './main-content.html',
+})
+export class MainContent {}
