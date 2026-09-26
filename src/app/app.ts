e47c8b8e33280f6ab2dab157a99548703ff809a6
Mir Elias Hashimi <elias.h.989@hotmail.com>
2026-09-26 14:12:29 +0200

    changed app.html and app.ts import Hero, changed hero.html and added classes for styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { Hero } from './layout/components/hero/hero';
 
 @Component({
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Hero],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
